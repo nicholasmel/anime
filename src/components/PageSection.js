c6eb80278af52462a6420cb90ffd799c2a253bd6
Nico Mel <nmel1@asu.edu>
2023-09-17 21:17:26 -0700

    - Update text color

--- src/components/PageSection.js
+++ src/components/PageSection.js
@@ -10,7 +10,7 @@ function PageSection({ title, quote }) {
         <div className="bg-black-900">
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-20 lg:flex lg:items-center lg:justify-between">
                 <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
-                    <span className="block text-slate-300 text-center lg:text-left">{title}</span>
+                    <span className="block text-white text-center lg:text-left">{title}</span>
                     <span className="block text-red-700 text-center lg:text-left">{quote}</span>
                 </h2>
                 <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0 items-center object-center justify-center">
